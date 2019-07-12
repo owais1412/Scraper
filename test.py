58369e7355f393726fc55039e28d595f2d6ccf89
owais1412 <owes.hussain@gmail.com>
2019-07-12 15:57:19 +0530

    First commit

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import scrapper
+import scraper
 
 url = 'https://www.amazon.in/Apple-iPhone-XR-128GB-Black/dp/B07JG7DS1T/ref=sr_1_3?keywords=iphone+xr&qid=1562926456&s=gateway&sr=8-3'
 
-scrapper.check_price(url=url)#Leave empty for iPhone Xs Max(64GB) Gold
+scraper.check_price(url=url)#Leave empty for iPhone Xs Max(64GB) Gold
